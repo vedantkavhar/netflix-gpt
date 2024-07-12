fcc80a58b0a907bab8b668fbd263da82e6756082
vedantkavhar <vedant.22110832@viit.ac.in>
2024-07-12 20:09:19 +0530

    first cra and tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">    
+        <p className='border border-blue-500 text-green-400 text-xl'>
+          Helo World!
+        </p>
+    </div>
+  );
+}
+
+export default App;
